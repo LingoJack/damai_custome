e5a595ceaaff6411ea3b352cdc53c1f739ccbaf4
shining-stars-lk <1091900093@qq.com>
2024-06-15 11:09:55 +0800

    增加基于基因法的雪花算法改造相关代码注释

--- damai-thread-pool-framework/src/main/java/com/damai/namefactory/AbstractNameThreadFactory.java
+++ damai-thread-pool-framework/src/main/java/com/damai/namefactory/AbstractNameThreadFactory.java
@@ -4,7 +4,7 @@ import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
- * @program: 极度真实还原大麦网高并发实战项目。 添加 阿星不是程序员 微信，添加时备注 大麦 来获取项目的完整资料 
+ * @program: 极度真实还原大麦网高并发实战项目。 添加 阿星不是程序员 微信，添加时备注 大麦 来获取项目的完整资料
  * @description: 线程工厂抽象基类
  * @author: 阿星不是程序员
  **/
@@ -17,10 +17,18 @@ public abstract class AbstractNameThreadFactory implements ThreadFactory {
 
 
     public AbstractNameThreadFactory() {
-        SecurityManager s = System.getSecurityManager();
-        group = (s != null) ? s.getThreadGroup() :
-                Thread.currentThread().getThreadGroup();
-        namePrefix = getNamePrefix() + "--thread--";
+        group = Thread.currentThread().getThreadGroup();
+
+        try {
+            String prefix = getNamePrefix();
+            if (prefix == null || prefix.isEmpty()) {
+                prefix = "default";
+            }
+            namePrefix = prefix + "--thread--";
+        }
+        catch (Exception e) {
+            namePrefix = "default--thread--";
+        }
     }
 
     /**
