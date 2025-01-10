80e7e662543c68c0e4b270fc810553dcd04e6318
LingoJack <3065225677@qq.com>
2025-01-10 21:29:48 +0800

    注释

--- damai-thread-pool-framework/src/main/java/com/damai/BusinessThreadPool.java
+++ damai-thread-pool-framework/src/main/java/com/damai/BusinessThreadPool.java
@@ -10,11 +10,8 @@ import java.math.RoundingMode;
 import java.util.concurrent.*;
 
 /**
- * @program: 极度真实还原大麦网高并发实战项目。 添加 阿星不是程序员 微信，添加时备注 大麦 来获取项目的完整资料
- * @description: 线程池
- * @author: 阿星不是程序员
+ * 线程池
  **/
-
 public class BusinessThreadPool extends BaseThreadPool {
 	// 线程池执行器
 	private static ThreadPoolExecutor execute = null;
